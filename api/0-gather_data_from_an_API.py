64c93c473cd0f667bc2615a3f71cf7ef65d78629
YanielRamos <yanielramos0504@gmail.com>
2023-08-22 13:41:00 -0400

    fixing

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,7 +4,7 @@ import requests
 from sys import argv
 
 
-def employee_task():
+if __name__ == '__main__':
     """function that count amount of task completed"""
     url_users = "https://jsonplaceholder.typicode.com/users"
     url_todos = "https://jsonplaceholder.typicode.com/todos"
@@ -35,10 +35,5 @@ def employee_task():
               .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
                       TOTAL_NUMBER_OF_TASKS))
 
-        print(TASK_TITLE)
         for task_printed in TASK_TITLE:
             print(f"\t {task_printed}")
-
-
-if __name__ == '__main__':
-    employee_task()
