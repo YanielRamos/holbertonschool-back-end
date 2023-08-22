1ce520b37c0a19a28731be7d4b2533de02a20119
YanielRamos <yanielramos0504@gmail.com>
2023-08-22 15:16:20 -0400

    file 2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,15 +5,15 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    """function that count amount of task completed"""
+    """script that count amount of task completed"""
     url_users = "https://jsonplaceholder.typicode.com/users"
     url_todos = "https://jsonplaceholder.typicode.com/todos"
     users_api = requests.get(url_users)
     todos_api = requests.get(url_todos)
-    TASK_TITLE = []
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    EMPLOYEE_NAME = ""
+    task_title = []
+    number_of_done_task = 0
+    total_number_of_tasks = 0
+    employee_name = ""
     id_employee = int(argv[1])
 
     if users_api.status_code == 200 and todos_api.status_code == 200:
@@ -22,18 +22,18 @@ if __name__ == '__main__':
 
         for user in users:
             if user['id'] == id_employee:
-                EMPLOYEE_NAME = user['name']
+                employee_name = user['name']
 
                 for task in todos:
                     if task['userId'] == id_employee:
-                        TOTAL_NUMBER_OF_TASKS += 1
+                        total_number_of_tasks += 1
                         if task['completed'] is True:
-                            NUMBER_OF_DONE_TASKS += 1
-                            TASK_TITLE.append(task['title'])
+                            number_of_done_task += 1
+                            task_title.append(task['title'])
 
         print("Employee {} is done with task({}/{}):"
-              .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
-                      TOTAL_NUMBER_OF_TASKS))
+              .format(employee_name, number_of_done_task,
+                      total_number_of_tasks))
 
-        for task_printed in TASK_TITLE:
+        for task_printed in task_title:
             print(f"\t {task_printed}")
---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""script that export data to json"""
+from sys import argv
+import requests
+import json
+
+
+if __name__ == '__main__':
+
+    user_id = int(argv[1])
+    users_response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+    todos_response = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id))
+    employee_name = ''
+
+    if users_response.status_code == 200 and todos_response.status_code == 200:
+        users = users_response.json()
+        todos = todos_response.json()
+
+        if users['id'] == user_id:
+            employee_name = users['username']
+
+        filename = f'{user_id}.json'
+        user_list = {user_id: []}
+
+        for info in todos:
+            dict = {
+                'task': info['title'], 'completed': info['completed'],
+                'username': employee_name}
+            user_list.get(user_id).append(dict)
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(user_list, f)
